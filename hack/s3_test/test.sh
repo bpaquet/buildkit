c4f992cba013b711f5a7cc729a2e383b839aeb04
Bertrand Paquet <bertrand.paquet@gmail.com>
2022-05-06 10:19:27 +0200

    Fixes
    Signed-off-by: Bertrand Paquet <bertrand.paquet@gmail.com>

--- hack/s3_test/test.sh
+++ hack/s3_test/test.sh
@@ -1,14 +1,22 @@
 #!/bin/sh -ex
 
-/bin/minio server /tmp/data --console-address=0.0.0.0:9001 &
-sleep 1
-mc alias set myminio http://172.17.0.2:9000 minioadmin minioadmin
+/bin/minio server /tmp/data --address=0.0.0.0:9000 --console-address=0.0.0.0:9001 &
+
+while true; do
+  curl -s -f http://127.0.0.1:9001 > /dev/null && break
+  sleep 1
+done
+
+sleep 2
+mc alias set myminio http://127.0.0.1:9000 minioadmin minioadmin
 mc mb myminio/my-bucket
 mc admin trace myminio &
 
 buildkitd -debugaddr 0.0.0.0:8060 &
-
-sleep 2
+while true; do
+  curl -s -f http://127.0.0.1:8060/debug/pprof/ > /dev/null && break
+  sleep 1
+done
 
 # First build: no cache on s3
 # 4 files should be exported (2 blobs + 2 manifests)
@@ -20,8 +28,8 @@ buildctl build \
   --import-cache "type=s3,bucket=my-bucket,name=foo,region=us-east-1,endpoint_url=http://127.0.0.1:9000,access_key_id=minioadmin,secret_access_key=minioadmin,s3_force_path_style=true" \
   --export-cache "type=s3,mode=max,bucket=my-bucket,name=foo;bar,region=us-east-1,endpoint_url=http://127.0.0.1:9000,access_key_id=minioadmin,secret_access_key=minioadmin,s3_force_path_style=true"
 
-# Check the 3 files are on s3
-mc ls --recursive myminio/my-bucket | wc -l | grep 4
+# Check the 5 files are on s3 (3 blobs and 2 manifests)
+mc ls --recursive myminio/my-bucket | wc -l | grep 5
 
 # Check we can reuse the cache
 buildctl prune
@@ -34,7 +42,7 @@ buildctl build \
   2>&1 | tee /tmp/log
 
 # Check the first step was not executed, but read from S3 cache
-cat /tmp/log | grep 'echo first' -A1 | grep CACHED
+cat /tmp/log | grep 'cat /dev/urandom | head -c 100 | sha256sum > unique_first' -A1 | grep CACHED
 
 # Test the behavior when a blob is missing
 mc rm --force --recursive myminio/my-bucket/blobs
